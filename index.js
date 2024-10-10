c516ec3ad25b71e53549cea8423c7396c6e8ed08
Hadi161 <hadi70037@gmail.com>
2024-10-09 20:22:07 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+   //task1
+   //Declaring a variable called Mynumber, and giving it the value of 10.
+let myNumber = 10;
+let myString = "Hello,Javascript!" ; //Declaring a variable called myString, and giving it the value of "Hello,Javascript"
+console.log(myNumber); 
+console.log(myString);
+
+//task2 part1
+
+for (let j = 0; j <= 10; j++) {
+    console.log(j);
+}
+
+//task2 part2 
+for (let j = 1; j <= 5; j++) {
+    console.log(3 * j);
+}
+
+//task3 
+function addNumbers(num1,num2) {
+    return num1 + num2;
+}
+
+let results = addNumbers(7, 10);
+console.log(results)
+
+
+function multiplyNumbers (num3,num4) {
+    return num3 * num4;
+}
+
+let outcome = multiplyNumbers(2, 9);
+console.log(outcome);
+
